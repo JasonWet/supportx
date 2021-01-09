550545278a6dd8b0bbc8250f04e3eef23e6ddee5
jasonwet <jason@colonialpress.net>
2021-01-09 14:45:37 -0500

    created links commands

---
+++ commands/links/website.js
@@ -0,0 +1,28 @@
+exports.run = async (client, message, args) => {
+    /*
+        Everything below this comment block is what will be run when a command is run.
+
+        Ex: If someone runs `!template` it will run this file having it send a message to the channel that the command was run in saying `This is a Template Command`
+     */
+    message.channel.send("https://savagelabs.net/")
+};
+
+/*
+    Command Configuration
+
+    THE conf & help ARE REQUIRED FOR ALL COMMANDS
+ */
+// Sets if a command is enabled, only able to be used in a guild and any aliases for the command
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+};
+
+// Sets the name, category, description and usage example for a command
+exports.help = {
+    name: "website",
+    category: "Links",
+    description: "SavageLabs Website Link",
+    usage: "website"
+};
