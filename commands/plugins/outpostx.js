75564c5ed6511df46b04e8e147d971c7dfe131f4
jasonwet <jason@colonialpress.net>
2021-01-09 14:37:40 -0500

    created plugin commands

---
+++ commands/plugins/outpostx.js
@@ -0,0 +1,28 @@
+exports.run = async (client, message, args) => {
+    /*
+        Everything below this comment block is what will be run when a command is run.
+
+        Ex: If someone runs `!template` it will run this file having it send a message to the channel that the command was run in saying `This is a Template Command`
+     */
+    message.channel.send("OutpostX")
+};
+
+/*
+    Command Configuration
+
+    THE conf & help ARE REQUIRED FOR ALL COMMANDS
+ */
+// Sets if a command is enabled, only able to be used in a guild and any aliases for the command
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: ['outpost'],
+};
+
+// Sets the name, category, description and usage example for a command
+exports.help = {
+    name: "outpostx",
+    category: "Plugins",
+    description: "OutpostX Plugin Information",
+    usage: "outpostx"
+};
