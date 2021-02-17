3a6f3415b4c7a9cfe672ccd18d38f14e67abe68f
JasonWet <jason@colonialpress.net>
2021-02-16 22:45:53 -0500

    added database variables

--- models/Guild.js
+++ models/Guild.js
@@ -2,6 +2,23 @@ const { Schema, model } =  require('mongoose');
 //Create a Schema
 const Guild = Schema({
     id: String,
+
+    ticketNumber: {
+        default: "0",
+        type: String,
+    },
+    suggestionNumber: {
+        default: "0",
+        type: String,
+    },
+
+    ticketMessage: String,
+    suggestionMessage: String,
+
+    ticketCategory: String,
+    suggestionCategory: String,
+
+    supportRole: String,
 });
 
 module.exports = model('Guild', Guild);
